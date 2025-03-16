c5c32519c8c50706b6a8b994d8770b84e699da69
mabdullahjs <mabdullah2037@outlook.com>
2025-03-16 12:47:33 +0500

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,7 @@
-// // // import React, { useEffect, useState } from 'react'
-
-// // // const App = () => {
-// // //   const [users , setUsers] = useState(null);
-// // //   useEffect(() => {
-// // //     fetch('https://jsonplaceholder.typicode.com/users')
-// // //       .then(response => response.json())
-// // //       .then(res => {
-// // //         console.log(res)
-// // //         setUsers(res)
-// // //       }).catch((err)=>{
-// // //         console.log(err);
-
-// // //       })
-// // //   }, [])
-// // //   return (
-// // //     <>
-// // //       <h1>Hello world</h1>
-
-// // //       {users ? users.map(item => {
-// // //         return <p key={item.id}>{item.name}</p>
-// // //       }) : <h1>Loading...</h1>}
-// // //     </>
-// // //   )
-// // // }
-
-// // // export default App
-
-
-// // // // falsy values in js
-// // // // 0
-// // // // ""
-// // // // null
-// // // // undefined
-// // // // Nan
-// // // // false
-// // // // document.all
-
-
-
-
-
-
-
-
-
-
-
-// // import axios from 'axios'
-// // import React, { useEffect, useState } from 'react'
-
-// // const App = () => {
-// //   const [loading , setLoading] = useState(true)
-// //   const [users , setUsers] = useState(null)
-// //   const [error , setError] = useState(false)
-// //   useEffect(()=>{
-// //     async function getData(){
-// //      try {
-// //        const response = await axios('https://jsonplaceholder.typicode.com/users')
-// //        console.log(response)
-// //        setUsers(response.data)
-// //      } catch (error) {
-// //       console.log('error ==> ' , error);
-// //       setError(true)
-
-// //      } finally {
-// //       console.log("finally");
-// //       setLoading(false);
-
-// //      }
-// //     }
-
-// //     getData()
-
-
-// //   } , [])
-// //   return (
-// //     <>
-// //       <h1>Hello world</h1>
-// //       {loading && <h1>Loading...</h1>}
-// //       {users && users.map(item => {
-// //         return <p key={item.id}>{item.name}</p>
-// //       })}
-// //       {error && <h1>Error occured</h1>}
-// //     </>
-// //   )
-// // }
-
-// // export default App
-
-
-
-
-
-
-
 import React, { useEffect, useState } from 'react'
 import Card from './components/Card'
 import axios from 'axios'
+import Logo from './assets/logo.jpg'
 
 const App = () => {
   const [products, setProducts] = useState(null);
@@ -119,6 +24,7 @@ const App = () => {
   return (
     <>
       <h1 className='text-center text-5xl bg-[#00bcff] text-white py-5 hover:bg-amber-300 hover:text-black cursor-pointer'>Hello world</h1>
+      <img src={Logo} alt="logoImg" />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error occured</h1>}
       <div style={{
@@ -139,30 +45,3 @@ const App = () => {
 
 export default App
 
-// // deployement
-// // tailwind
-// // bootstrap
-// // daisy UI
-// // shadcn
-// // material
-// // react router dom
-// // usecallback and use memo
-// // supabase
-// // redux toolkit
-// // project week
-// // next js (2)
-// // react native
-
-
-
-// import React from 'react'
-
-// const App = () => {
-//   return (
-//     <>
-//     <h1 className='text-center text-5xl bg-[#00bcff] text-white p-[10rem] m-[50px] hover:bg-amber-300 hover:text-black cursor-pointer'>Hello world</h1>
-//     </>
-//   )
-// }
-
-// export default App
